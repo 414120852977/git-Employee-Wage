5325b22dfdf76f3e30158c9a0dcee4486e619e2c
Ashok Mane <ashokmane81800@gmail.com>
2021-03-09 18:37:24 +0530

    [ashok] created employee is present or abscent in employeewage.sh

--- employeewage.sh
+++ employeewage.sh
@@ -1,2 +1,17 @@
 #! /bin/bash -x
 echo "Welcome Emplyees Wage Computation"
+
+is_present=1
+is_abscent=0
+
+for(( i=0; i<5; i++ ))
+do
+	e=$(( RANDOM%2 ))
+
+	if [ $(( e )) -eq $i ]
+	then
+		echo "Employee is present"
+	else
+		echo "Emplyee not present"
+	fi
+done
